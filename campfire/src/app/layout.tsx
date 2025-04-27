ecc2fae2ec487b6d14b0dab2ec27ae306397ae3a
doctorthe113 <code.doc113@gmail.com>
2025-04-27 21:35:19 +0600

    added robot.txt, added a lot more meta tags

--- campfire/src/app/layout.tsx
+++ campfire/src/app/layout.tsx
@@ -19,15 +19,49 @@ const merriweather = Merriweather({
 export const metadata: Metadata = {
     title: `Campfire ${process.env.NODE_ENV === "development" ? "- dev" : ""}`,
     description: "A simple discord like chatapp",
+    authors: [{ name: "Doctorthe113" }],
+    keywords: [
+        "discord",
+        "chatapp",
+        "campfire",
+        "discord clone",
+        "campfire.doctorthe113.com",
+        "discord.doctorthe113.com",
+    ],
     openGraph: {
         title: `Campfire ${
             process.env.NODE_ENV === "development" ? "- dev" : ""
         }`,
         description: "A simple discord like chatapp",
         siteName: "Campfire",
-        images: [],
+        images: "https://campfire.doctorthe113.com/campfire.webp",
         locale: "en-US",
         type: "website",
+        url: "https://campfire.doctorthe113.com",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+    icons: {
+        shortcut: "/favicon.ico",
+        apple: "/campfire.webp",
+    },
+    twitter: {
+        title: "Campfire",
+        description: "A simple discord like chatapp",
+        images: "https://campfire.doctorthe113.com/campfire.webp",
+        creator: "@doctorthe113",
+        site: "campfire.doctorthe113.com",
     },
 };
 
